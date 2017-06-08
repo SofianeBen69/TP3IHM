9c18a29f98d66f211eab981f2143b7f306311b72
sofiane <benslimanesofiane@hotmail.fr>
2017-06-08 09:50:45 +0200

    Ajout class Main

---
+++ src/main/TP3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import Source.GestionProduit;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author sofiane
+ */
+
+
+public class TP3 {
+    public static void main(String args[])
+    {
+        JFrame frame = new GestionProduit();
+        frame.setVisible(true);
+    }
+}
